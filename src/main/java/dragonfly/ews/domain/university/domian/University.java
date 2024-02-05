5489229e7624b5a3ed8db3cfc4acc7943b457e0f
useongee <118250509+USEONGEE@users.noreply.github.com>
2024-02-05 21:15:31 +0900

    feat: University 도메인

--- src/main/java/dragonfly/ews/domain/university/domian/University.java
+++ src/main/java/dragonfly/ews/domain/university/domian/University.java
@@ -1,4 +1,20 @@
 package dragonfly.ews.domain.university.domian;
 
+import dragonfly.ews.domain.department.domain.Department;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class University {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "university", cascade = CascadeType.ALL)
+    private List<Department> departments = new ArrayList<>();
+
+
 }
