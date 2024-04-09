40452ee622397370dfef2c269d8f3e15e50e9b0e
YUSEONG NOH <118250509+USEONGEE@users.noreply.github.com>
2024-04-09 12:16:52 +0900

    fix: error

--- src/main/java/dragonfly/ews/domain/filelog/controller/ExcelMemberFileLogControllerAdvice.java
+++
@@ -1,7 +0,0 @@
-package dragonfly.ews.domain.filelog.controller;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice(assignableTypes = ExcelMemberFileLogController.class)
-public class ExcelMemberFileLogControllerAdvice {
-}
